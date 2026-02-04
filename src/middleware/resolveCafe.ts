e0b9eca5a5208ade3c5df18117406402854e9b4e
mufaddal-viit <mufaddal@pamcorpservices.com>
2026-02-04 23:30:32 +0400

    render changes

--- src/middleware/resolveCafe.ts
+++ src/middleware/resolveCafe.ts
@@ -4,7 +4,8 @@ import { asyncHandler } from "../utils/asyncHandler";
 import { badRequest, forbidden, notFound } from "../utils/errors";
 
 export const resolveCafe: RequestHandler = asyncHandler(async (req, _res, next) => {
-  const cafeSlug = req.params.cafeSlug;
+  const slugParam = req.params.cafeSlug;
+  const cafeSlug = Array.isArray(slugParam) ? slugParam[0] : slugParam;
 
   if (!cafeSlug) {
     throw badRequest("Cafe slug is required");
